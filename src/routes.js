a7d1c696b3ebba20bd261b65345b306d574c1caf
LeVon <levonstepanyan.ait@gmail.com>
2022-03-30 15:42:10 +0400

    feat(app) : add first routes

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,14 @@
 // components
 import Main from "./pages/Main";
+import Register from "./pages/Register";
 
 export const routes = [
-    {
-        path : '/',
-        component : Main,
-    },
-]
+  {
+    path: "/",
+    component: Main,
+  },
+  {
+      path : "/register",
+      component : Register,
+  },
+];
